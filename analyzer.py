113c891edb045a6e9ea2dc9d72250084b2493a18
jonasjuerss <jonas.juerss@tum.de>
2023-05-13 17:04:02 +0100

    Added batching support for analyzer, allowing to deal with RedditBinary

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,7 @@
+import math
 from dataclasses import dataclass
 from functools import partial
-from typing import Optional, Type, List
+from typing import Optional, Type, List, Sequence
 
 import matplotlib
 import networkx as nx
@@ -12,11 +13,13 @@ import torch
 from sklearn.base import BaseEstimator
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
-from torch_geometric.data import DataLoader, Data
-from torch_geometric.loader import DenseDataLoader
+from torch.utils.data import Subset
+from torch_geometric.data import Data
+from torch_geometric.loader import DenseDataLoader, DataLoader
 from torch_geometric.utils import k_hop_subgraph
 from torch_scatter import scatter
 from sklearn.tree import DecisionTreeClassifier
+from tqdm import tqdm
 
 from clustering_wrappers import ClusterAlgWrapper
 from custom_net import InferenceInfo
@@ -28,14 +31,18 @@ import custom_logger
 
 
 class Analyzer():
-    def __init__(self, wandb_id: str, resume_last: bool, train_loader: DataLoader = None, test_loader: DataLoader = None):
-        _train_loader, _val_loader, _test_loader = self.load_model(wandb_id, resume_last)
-        self.train_data, self.train_out, self.train_concepts, train_info, self.train_y_pred =\
-            self.load_whole_dataset(_train_loader if train_loader is None else train_loader)
-        self.test_data, self.test_out, self.test_concepts, test_info, self.test_y_pred = \
-            self.load_whole_dataset(_test_loader if test_loader is None else test_loader)
-        self.train_info: InferenceInfo = train_info
-        self.test_info: InferenceInfo = test_info
+    def __init__(self, wandb_id: str, resume_last: bool, load_part: float = 1, train_loader: DataLoader = None, test_loader: DataLoader = None, val_loader: DataLoader = None, **overwrite_args):
+        _train_loader, _val_loader, _test_loader = self.load_model(wandb_id, resume_last=resume_last, **overwrite_args)
+        self.train_loader = _train_loader if train_loader is None else train_loader
+        self.test_loader = _test_loader if test_loader is None else test_loader
+        self.val_loader = _val_loader if val_loader is None else val_loader
+
+        # self.train_data, self.train_out, self.train_concepts, train_info, self.train_y_pred = \
+        #     self.load_whole_dataset(_train_loader if train_loader is None else train_loader, load_part, "train")
+        # self.test_data, self.test_out, self.test_concepts, test_info, self.test_y_pred = \
+        #     self.load_whole_dataset(_test_loader if test_loader is None else test_loader, load_part, "test")
+        # self.train_info: InferenceInfo = train_info
+        # self.test_info: InferenceInfo = test_info
 
         #self.colors = np.array([matplotlib.colors.rgb2hex(c) for c in pylab.get_cmap("tab20").colors])
         self.input_dim : int = self.model.graph_network.pool_blocks[0].input_dim
@@ -43,15 +50,18 @@ class Analyzer():
             ['#2c3e50', '#e74c3c', '#27ae60', '#3498db', '#CDDC39', '#f39c12', '#795548', '#8e44ad', '#3F51B5',
              '#7f8c8d', '#e84393', '#607D8B', '#8e44ad', '#009688'])
 
-    def load_model(self, wandb_id: str, resume_last: bool):
+    def load_model(self, wandb_id: str,**overwrite_args):
         self.model, self.config, self.dataset_wrapper, train_loader, val_loader, test_loader =\
             main(dict(resume=wandb_id, save_path="models/dummy.pt"), use_wandb=False, num_epochs=0, shuffle=False,
-                 resume_last=resume_last)
+                 **overwrite_args)
         self.model.eval()
         return train_loader, val_loader, test_loader
 
-    def load_whole_dataset(self, data_loader: DataLoader):
-        data_loader = data_loader.__class__(data_loader.dataset, batch_size=len(data_loader.dataset), shuffle=False)
+    def load_whole_dataset(self, data_loader: DataLoader, load_part: float, mode: str):
+        num_samples = math.floor(load_part * len(data_loader.dataset))
+        data_loader = data_loader.__class__(data_loader.dataset,
+                                            batch_size=num_samples,
+                                            shuffle=False)
         # extract the first (and only) data batch
         for data in data_loader:
             pass
@@ -59,8 +69,117 @@ class Analyzer():
         with torch.no_grad():
             out, _, concepts, _, info = self.model(data, collect_info=True)
         y_pred = torch.argmax(out, dim=1)
-        print(f"Accuracy: {100 * torch.sum(y_pred == data.y.squeeze(-1)) / y_pred.shape[0]:.2f}%")
+        print(f"Loaded {num_samples} {mode} samples. Accuracy {100 * torch.sum(y_pred == data.y.squeeze(-1)) / y_pred.shape[0]:.2f}%")
         return data, out, concepts, info, y_pred
+
+    @staticmethod
+    def cat_pad(inputs: Sequence[torch.Tensor], dim: int, fill_value: float = 0):
+        # Alternatively could use: https://github.com/pytorch/pytorch/issues/10978
+        res_shape = tuple(max(inp.shape[i] for inp in inputs) for i in range(dim)) + \
+                    (sum([inp.shape[dim] for inp in inputs]),) + \
+                    tuple(max(inp.shape[i] for inp in inputs) for i in range(dim + 1, inputs[0].ndim))
+        res = torch.full(res_shape, fill_value=fill_value, device=inputs[0].device, dtype=inputs[0].dtype)
+        start = 0
+        for i, inp in enumerate(inputs):
+            res[tuple(slice(0, inp.shape[j]) for j in range(dim)) + (slice(start, start + inp.shape[dim]),)
+                + tuple(slice(0, inp.shape[j]) for j in range(dim + 1, inputs[0].ndim))] = inp
+            start += inp.shape[dim]
+        return res
+
+
+    def load_required_data(self, data_loader: DataLoader, load_part: float, mode: str, load_names: List[str]):
+        set_size = math.floor(load_part * len(data_loader.dataset))
+        data_loader = data_loader.__class__(data_loader.dataset[:set_size], data_loader.batch_size)
+        num_classes = self.dataset_wrapper.num_classes
+        max_nodes = self.dataset_wrapper.max_nodes_per_graph
+        num_node_features = self.dataset_wrapper.num_node_features
+        res = dict()
+        if "y_pred" in load_names:
+            res["y_pred"] = torch.empty(set_size, dtype=torch.long)
+        if "target" in load_names:
+            res["target"] = torch.empty(set_size, dtype=torch.long)
+        if "out" in load_names:
+            res["out"] = torch.empty(set_size, num_classes)
+        if "x" in load_names:
+            if self.config.dense_data:
+                res["x"] = torch.full((set_size, max_nodes, num_node_features), float('nan'))
+            else:
+                res["x"] = torch.empty(self.dataset_wrapper.num_nodes_total, num_node_features)
+        if "adj" in load_names:
+            res["adj"] = torch.zeros(set_size, max_nodes, max_nodes, dtpye=torch.int)
+        if "edge_index" in load_names:
+            res["edge_index"] = torch.zeros(2, self.dataset_wrapper.num_edges_total, dtpye=torch.int)
+        if "mask" in load_names:
+            res["mask"] = torch.zeros(set_size, max_nodes, dtpye=torch.bool)
+        if "batch" in load_names:
+            res["batch"] = torch.zeros(set_size, dtpye=torch.long)
+
+        sample_i = 0
+        node_i = 0
+        edge_i = 0
+        correct = 0
+        with torch.no_grad():
+            for batch_i, data in tqdm(enumerate(data_loader), total=math.ceil(set_size / data_loader.batch_size)):
+                target = data.y
+                if self.config.dense_data:
+                    target = target.squeeze(1)
+                sample_i_new = sample_i + target.shape[0]
+                if "target" in load_names:
+                    res["target"][sample_i:sample_i_new] = target
+
+                target = target.to(custom_logger.device)
+
+                if self.config.dense_data:
+                    if "x" in load_names:
+                        res["x"][sample_i:sample_i_new] = data.x
+                    if "adj" in load_names:
+                        res["adj"][sample_i:sample_i_new] = data.adj
+                    if "mask" in load_names:
+                        res["mask"][sample_i:sample_i_new] = data.mask
+                else:
+                    if "x" in load_names:
+                        res["x"][node_i:node_i + data.x.shape[1]] = data.x
+                    if "edge_index" in load_names:
+                        res["edge_index"][:, edge_i:edge_i + data.edge_index.shape[1]] = data.edge_index
+                    if "batch" in load_names:
+                        res["batch"][node_i:node_i + data.x.shape[1]] = data.batch
+                    node_i += data.x.shape[0]
+                    edge_i += data.edge_index.shape[1]
+
+                data.to(custom_logger.device)
+                out, _, concepts, _, info = self.model(data, collect_info=True)
+                y_pred = torch.argmax(out, dim=1)
+
+                correct += torch.sum(y_pred == target).item()
+
+                if "y_pred" in load_names:
+                    res["y_pred"][sample_i:sample_i_new] = y_pred.cpu()
+                if "out" in load_names:
+                    res["out"][sample_i:sample_i_new] = out.cpu()
+
+                if "concepts" in load_names:
+                    if "concepts" in res:
+                        res["concepts"] = torch.cat((res["concepts"], concepts), dim=0)
+                    else:
+                        res["concepts"] = concepts
+
+                for key in ["pooling_assignments", "pooling_activations", "adjs_or_edge_indices", "all_batch_or_mask",
+                            "input_embeddings"]:
+                    # TODO support sparse
+                    if key in load_names:
+                        if "info_" + key in res:
+                            for i, e in enumerate(getattr(info, key)):
+                                if e is not None:
+                                    res["info_" + key][i] = self.cat_pad((res["info_" + key][i], e.cpu()), dim=0,
+                                                                         fill_value=-1 if key == "pooling_assignments" else 0)
+
+                        else:
+                            res["info_" + key] = [None if e is None else e.cpu() for e in getattr(info, key)]
+
+                sample_i = sample_i_new
+
+        print(f"Loaded {set_size} {mode} samples. Accuracy {100 * correct / set_size:.2f}%")
+        return res # data, out, concepts, info, y_pred
 
     @staticmethod
     def _decision_tree_acc(X_train: np.ndarray, Y_train: np.ndarray, X_test: Optional[np.ndarray] = None,
@@ -77,34 +196,50 @@ class Analyzer():
         tree.fit(X_train, Y_train)
         return tree.score(X_test if X_test is not None else X_train, Y_test if Y_test is not None else Y_train)
 
-    def calculate_concept_completeness(self, multiset: bool = True) -> List[float]:
+    def calculate_concept_completeness(self, multiset: bool = True, data_part: float = 1.0, seeds: List[int] = [0]) ->\
+            torch.Tensor:
         """
         Important: For the inputs, this assumes one-hot encoding
         """
-        enumerate_feat = 2 ** torch.arange(self.train_data.x.shape[-1], device=self.train_data.x.device)[None, None, :]
-        all_train_assignments = [torch.sum(self.train_data.x.int() * enumerate_feat, dim=-1)] +\
-                                self.train_info.pooling_assignments
-        all_test_assignments = [torch.sum(self.test_data.x.int() * enumerate_feat, dim=-1)] +\
-                               self.test_info.pooling_assignments
-        result = []
-        for pool_step, (train_assignments, test_assignments) in enumerate(zip(all_train_assignments,
-                                                                              all_test_assignments)):
-            if train_assignments is None:
-                continue  # For non-MonteCarlo layers
-            num_concepts = max(torch.max(train_assignments), torch.max(test_assignments)) + 1
-            batched_bincount = vmap(partial(torch.bincount, minlength=num_concepts + 1))
+        results = []
+        for seed in seeds:
+            torch.manual_seed(seed)
+            np.random.seed(seed)
+            train_data = self.load_required_data(self.train_loader, data_part, "train",
+                                                 ["x", "target", "pooling_assignments"])
+            test_data = self.load_required_data(self.test_loader, data_part, "test",
+                                                 ["x", "target", "pooling_assignments"])
+            enumerate_feat = 2 ** torch.arange(train_data["x"].shape[-1],
+                                               device=train_data["x"].device)[None, None, :]
+            all_train_assignments = [torch.sum(train_data["x"].int() * enumerate_feat, dim=-1)] +\
+                                    train_data["info_pooling_assignments"]
+            all_test_assignments = [torch.sum(test_data["x"].int() * enumerate_feat, dim=-1)] +\
+                                   test_data["info_pooling_assignments"]
+            result = []
+            for pool_step, (train_assignments, test_assignments) in enumerate(zip(all_train_assignments,
+                                                                                  all_test_assignments)):
+                if train_assignments is None:
+                    continue  # For non-MonteCarlo layers
+                num_concepts = max(torch.max(train_assignments), torch.max(test_assignments)) + 1
+                batched_bincount = vmap(partial(torch.bincount, minlength=num_concepts + 1))
 
-            # [batch_size, num_concepts] (note that assignments contains -1 for masked values)
-            multisets_train = batched_bincount(train_assignments + 1)[:, 1:]
-            multisets_test = batched_bincount(test_assignments + 1)[:, 1:]
+                # [batch_size, num_concepts] (note that assignments contains -1 for masked values)
+                multisets_train = batched_bincount(train_assignments + 1)[:, 1:]
+                multisets_test = batched_bincount(test_assignments + 1)[:, 1:]
 
-            if not multiset:
-                multisets_train = multisets_train.bool().int()
-                multisets_test = multisets_test.bool().int()
+                if not multiset:
+                    multisets_train = multisets_train.bool().int()
+                    multisets_test = multisets_test.bool().int()
 
-            result.append(self._decision_tree_acc(multisets_train.cpu(), self.train_data.y.squeeze().cpu(),
-                                                  multisets_test.cpu(), self.test_data.y.squeeze().cpu()))
-        return result
+                result.append(self._decision_tree_acc(multisets_train.cpu(), train_data["target"].squeeze(),
+                                                      multisets_test.cpu(), test_data["target"].squeeze()))
+            results.append(result)
+        # [num_seeds, num_mc_blocks + 1]
+        results = torch.tensor(results)
+        stds, means = torch.std_mean(results, dim=0)
+        for i in range(stds.shape[0]):
+            print(f"{100*means[i]:.2f}%+-{100*stds[i]:.2f}")
+        return results
 
         # dataset_instance = self.dataset_wrapper.get_dataset(self.config.dense_data, 0)
         # if isinstance(self.dataset_wrapper, CustomDatasetWrapper):
@@ -161,21 +296,21 @@ class Analyzer():
                 fig.savefig(f"{save_path}_{i}.pdf", bbox_inches='tight')
 
     ###################################################### Legacy ######################################################
-    def plot_clusters(self, K: int):
-        # [num_nodes_total, x, y] (PCA or t-SNE)
-        coords = TSNE(n_components=2).fit_transform(X=self.x_out_all)
-        kmeans = KMeans(n_clusters=K).fit(X=self.x_out_all)
-        # [num_nodes_total] with integer values between 0 and K/num_clusters/num_concepts
-        clusters = kmeans.labels_
-
-        markers = ["o", "p", "s", "P", "*", "D", "^", "+", "x"]
-
-        fig, ax = plt.subplots()
-        for i in range(self.model.output_dim):
-            # Note, we could also evaluate how the model classifies (y_pred_all) instead of the ground truth (y_all) for explainability
-            ax.scatter(coords[self.y_all_nodes == i, 0], coords[self.y_all_nodes == i, 1],
-                       c=self.colors[clusters[self.y_all_nodes == i]],
-                       marker=markers[i], s=10)
+    # def plot_clusters(self, K: int):
+    #     # [num_nodes_total, x, y] (PCA or t-SNE)
+    #     coords = TSNE(n_components=2).fit_transform(X=self.x_out_all)
+    #     kmeans = KMeans(n_clusters=K).fit(X=self.x_out_all)
+    #     # [num_nodes_total] with integer values between 0 and K/num_clusters/num_concepts
+    #     clusters = kmeans.labels_
+    #
+    #     markers = ["o", "p", "s", "P", "*", "D", "^", "+", "x"]
+    #
+    #     fig, ax = plt.subplots()
+    #     for i in range(self.model.output_dim):
+    #         # Note, we could also evaluate how the model classifies (y_pred_all) instead of the ground truth (y_all) for explainability
+    #         ax.scatter(coords[self.y_all_nodes == i, 0], coords[self.y_all_nodes == i, 1],
+    #                    c=self.colors[clusters[self.y_all_nodes == i]],
+    #                    marker=markers[i], s=10)
 
     def draw_neighbourhood(self, ax, node_index: int):
         subset, edge_index, mapping, _ = k_hop_subgraph(node_index, self.num_hops, torch.tensor(self.edge_index_all),
--- data_generation/dataset_wrappers.py
+++ data_generation/dataset_wrappers.py
@@ -1,4 +1,5 @@
 import abc
+import traceback
 from typing import Optional, List, Type
 
 from torch_geometric.data import Dataset
@@ -11,13 +12,16 @@ from data_generation.transforms import RemoveEdgeFeatures
 
 
 class DatasetWrapper(seri.ArgSerializable, abc.ABC):
-    def __init__(self, num_classes: int, num_node_features: int, is_directed: bool, args: dict,
+    def __init__(self, num_classes: int, num_node_features: int, is_directed: bool, max_nodes_per_graph: int, args: dict,
                  class_names: Optional[List[str]] = None):
         super().__init__(args)
         self._dataset = None
         self.num_classes = num_classes
         self.num_node_features = num_node_features
         self.is_directed = is_directed
+        self.max_nodes_per_graph = max_nodes_per_graph
+        self._num_nodes_total = None
+        self._num_edges_total = None
         if class_names is None:
             self.class_names = [f"Class {i}" for i in range(num_classes)]
         else:
@@ -34,11 +38,37 @@ class DatasetWrapper(seri.ArgSerializable, abc.ABC):
             self._dataset = self._get_dataset(dense, min_nodes)
         return self._dataset
 
+    @property
+    def num_nodes_total(self) -> int:
+        """
+        Important: note that this will shuffle the dataset
+        """
+        if self._dataset is None:
+            raise RuntimeError("Number of nodes cannot be calculated as dataset hasn't been generated yet.")
+        if self._num_nodes_total is None:
+            self._num_nodes_total = sum([d.num_nodes for d in self._dataset])
+        return self._num_nodes_total
+
+    @property
+    def num_edges_total(self) -> int:
+        """
+        Important: note that this will shuffle the dataset
+        """
+        if self._dataset is None:
+            raise RuntimeError("Number of edges cannot be calculated as dataset hasn't been generated yet.")
+        if self._num_edges_total is None:
+            try:
+                self._num_edges_total = sum([d.edge_index.shape[0] for d in self._dataset])
+            except:
+                print("Could not get number of edges. Note that this is only possible for sparse data.")
+                traceback.print_exc()
+        return self._num_edges_total
+
 
 class CustomDatasetWrapper(DatasetWrapper, abc.ABC):
 
     def __init__(self, sampler: CustomDataset, num_samples=4*(512 + 128)):
-        super().__init__(sampler.num_classes, sampler.num_node_features, False,
+        super().__init__(sampler.num_classes, sampler.num_node_features, False, sampler.max_nodes,
                          dict(sampler=sampler, num_samples=num_samples),
                          sampler.class_names)
         self.sampler = sampler
@@ -56,7 +86,8 @@ class PyGWrapper(DatasetWrapper, abc.ABC):
     def __init__(self, dataset_class: Type[Dataset], remove_edge_fts: bool, is_directed: bool, dataset_kwargs: dict,
                  args: dict, class_names: Optional[List[str]] = None):
         self.dummy_dataset = dataset_class(**dataset_kwargs)
-        super().__init__(self.dummy_dataset.num_classes, self.dummy_dataset.num_node_features, is_directed, args,
+        super().__init__(self.dummy_dataset.num_classes, self.dummy_dataset.num_node_features, is_directed,
+                         max([d.num_nodes for d in self.dummy_dataset]), args,
                          class_names)
         self.dataset_class = dataset_class
         self.dataset_kwargs = dataset_kwargs
@@ -79,8 +110,7 @@ class PyGWrapper(DatasetWrapper, abc.ABC):
             transforms.append(RemoveEdgeFeatures())
 
         if dense:
-            max_nodes = max([d.num_nodes for d in self.dummy_dataset])
-            transforms.append(ToDense(num_nodes=max_nodes))
+            transforms.append(ToDense(num_nodes=self.max_nodes_per_graph))
 
         if len(transforms) > 0:
             kwargs["transform"] = Compose(transforms) if len(transforms) > 1 else transforms[0]
@@ -90,6 +120,7 @@ class PyGWrapper(DatasetWrapper, abc.ABC):
         dataset = dataset.shuffle()
         return dataset
 
+
 class TUDatasetWrapper(PyGWrapper):
     def __init__(self, dataset_name: str, is_directed: bool, remove_edge_fts: bool = False, args=None,
                  class_names: Optional[List[str]] = None):
@@ -97,21 +128,26 @@ class TUDatasetWrapper(PyGWrapper):
             args = dict(dataset_name=dataset_name)
         super().__init__(TUDataset, remove_edge_fts, is_directed, dict(root='/tmp', name=dataset_name), args, class_names)
 
+
 class MutagWrapper(TUDatasetWrapper):
     def __init__(self, remove_edge_fts: bool = True):
         super().__init__("MUTAG", False, remove_edge_fts, dict(remove_edge_fts=remove_edge_fts), ["not mutagenic", "mutagenic"])
+
 
 class MutagenicityWrapper(TUDatasetWrapper):
     def __init__(self, remove_edge_fts: bool = True):
         super().__init__("Mutagenicity", False, remove_edge_fts, dict(remove_edge_fts=remove_edge_fts))
 
+
 class RedditBinaryWrapper(TUDatasetWrapper):
     def __init__(self):
         super().__init__("REDDIT-BINARY", is_directed=False, args=dict())
 
+
 class EnzymesWrapper(TUDatasetWrapper):
     def __init__(self):
         super().__init__("ENZYMES", is_directed=False, args=dict())
+
 
 class ProteinsWrapper(TUDatasetWrapper):
     def __init__(self):
